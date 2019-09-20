c28c5645a29046076217148a846b825979b7ec51
Josephdicarlocraig <joey@dicarlocraig.net>
2019-09-20 20:24:01 +0000

    create awesome platformer game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,8 +38,27 @@
         platform.create(400, 460);
         platform.create(500, 200);
         
+        platform.create(x, y, scaleX, scaleY);
+ 
+            x: The x coordineate for the platform.
+            y: The y coordineate for the platform.
+            scaleX: OPTIONAL The scale factor on the x-axis, this value will stretch the platform in width.
+            scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the platform in height.
         
-        
+        collectable.create(type.Tim, 200, 170, 6, .7);
+
+            type: The type of the collectable
+            x: The x coordineate for the collectable.
+            y: The y coordineate for the collectable.
+            gravity: OPTIONAL The gravitational pull on the collectable.
+            bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+            
+                cannon.create.onTop(450);
+                cannon.create.onBottom(225);
+                cannon.create.onLeft(300);
+                cannon.create.onRight(110); 
+                
+                
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     };
